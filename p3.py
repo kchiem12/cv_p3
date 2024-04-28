5dd6a7c76153bd30f625766a2bf2f9d01be349d8
Kenneth Chiem <kc696@cornell.edu>
2024-04-28 13:48:52 -0400

    fix rounding issue

--- p3.py
+++ p3.py
@@ -8,18 +8,19 @@ from scipy import ndimage, signal
 
 # TODO 1: Read an Image and convert it into a floating point array with values between 0 and 1. You can assume a color image
 def imread(filename):
-    image = np.array(Image.open(filename))
+    image = np.array(Image.open(filename))[:,:,:3]
 
-    return image.astype(np.float32) / 255
+    return image.astype(float) / 255.0
 
 # TODO 2: Create a gaussian filter of size k x k and with standard deviation sigma
-
-
 def gaussian_filter(k, sigma):
-    x, y = np.meshgrid(np.linspace(-k // 2, k // 2, k),
-                       np.linspace(-k // 2, k // 2, k))
-    g = np.exp(-((x**2 + y**2)/(2.0*sigma**2)))
-    return g / g.sum()
+    ax = np.arange(-k // 2 + 1, k // 2 + 1)
+    xx, yy = np.meshgrid(ax, ax)
+
+    # Calculate the Gaussian matrix
+    kernel = np.exp(-(xx**2 + yy**2) / (2 * sigma**2))
+    kernel /= np.sum(kernel)  # Normalize the kernel
+    return kernel
 
 # TODO 3: Compute the image gradient.
 # First convert the image to grayscale by using the formula:
@@ -27,8 +28,6 @@ def gaussian_filter(k, sigma):
 # Then convolve with a 5x5 Gaussian with standard deviation 1 to smooth out noise. (use scipy.signal.convolve)
 # Convolve with [0.5, 0, -0.5] to get the X derivative on each channel and convolve with [[0.5],[0],[-0.5]] to get the Y derivative on each channel. (use scipy.signal.convolve)
 # Return the gradient magnitude and the gradient orientation (use arctan2)
-
-
 def gradient(img):
     grayscale = 0.2125 * img[:, :, 0] + 0.7154 * \
         img[:, :, 1] + 0.0721 * img[:, :, 2]
@@ -55,16 +54,14 @@ def gradient(img):
 # The input x and y are arrays representing the x and y coordinates of each pixel
 # Return a boolean array that indicates True for pixels whose distance is less than the threshold
 def check_distance_from_line(x, y, theta, c, thresh):
-    def func(x): return -((x*np.cos(theta) + c)/np.sin(theta))
-    yline = func(x)
-    dist = np.sqrt((x-x)**2 + (y-yline)**2)
+    func = x*np.cos(theta) + y*np.sin(theta) + c
+    dist = np.abs(func)
     return dist < thresh
 
 
 # TODO 5: Write a function to draw a set of lines on the image. The `lines` input is a list of (theta, c) pairs. Each line must appear as red on the final image
 # where every pixel which is less than thresh units away from the line should be colored red
 def draw_lines(img, lines, thresh):
-
     copy = np.copy(img)
     rows, cols, _ = copy.shape
 
@@ -72,8 +69,10 @@ def draw_lines(img, lines, thresh):
         for x in range(cols):
 
             for theta, c in lines:
-                if check_distance_from_line([x], [y], theta, c, thresh)[0]:
-                    copy[y, x] = (1, 0, 0)
+                if check_distance_from_line(x, y, theta, c, thresh):
+                    copy[y, x] = [1, 0, 0]
+
+    return copy
 
 
 # TODO 6: Do Hough voting. You get as input the gradient magnitude and the gradient orientation, as well as a set of possible theta values and a set of possible c
@@ -83,20 +82,36 @@ def draw_lines(img, lines, thresh):
 # (c) The difference between theta and the pixel's gradient orientation is less than thresh3
 def hough_voting(gradmag, gradori, thetas, cs, thresh1, thresh2, thresh3):
     arr = np.zeros((len(thetas), len(cs)))
-    for i in range(gradmag.shape[0]):
-        for j in range(gradmag.shape[1]):
-            if gradmag[i, j] > thresh1:
-                for k in range(len(thetas)):
-                    for l in range(len(cs)):
-                        if check_distance_from_line(i, j, thetas[k], cs[l], thresh2) and np.abs(gradori[i, j]-thetas[k]) < thresh3:
-                            arr[k, l] += 1
+
+    cos_thetas = np.cos(thetas)
+    sin_thetas = np.sin(thetas)
+
+    # filter pixels based on gradient magnitude
+    y_idxs, x_idxs = np.nonzero(gradmag > thresh1)
+    valid_orientations = gradori[y_idxs, x_idxs]
+
+    # Iterate through each theta and c pair
+    for k, (cos_theta, sin_theta) in enumerate(zip(cos_thetas, sin_thetas)):
+        # Compute the distance from the line for all filtered pixels
+        distances = x_idxs * cos_theta + y_idxs * sin_theta + cs[:, np.newaxis]
+        close_to_line = np.abs(distances) < thresh2
+
+        # Check if orientation difference is less than thresh3
+        orientation_diff = np.abs(valid_orientations - thetas[k]) < thresh3
+
+        # Combine conditions and count votes
+        for l, c_close_to_line in enumerate(close_to_line):
+            votes = np.sum(c_close_to_line & orientation_diff)
+            if votes:
+                arr[k, l] += votes
+
+    return arr
 
 
 # TODO 7: Find local maxima in the array of votes. A (theta, c) pair counts as a local maxima if (a) its votes are greater than thresh, and
 # (b) its value is the maximum in a (nbhd x nbhd) neighborhood in the votes array.
 # Return a list of (theta, c) pairs
 def localmax(votes, thetas, cs, thresh, nbhd):
-
     final = []
 
     max_filtered = ndimage.maximum_filter(votes, size=nbhd)
